0422b9394dbf61ac2aa3f1f8d913a43d8ad32e8c
Will Gries <wgries@microsoft.com>
2020-01-02 16:14:36 -0800

    Kick off configure script from ARM template.

---
+++ dns-forwarder/configureVM.ps1
@@ -0,0 +1,69 @@
+param(
+    [switch]$DomainJoin,
+    [string]$DomainName,
+    [string]$OdjBlob,
+    [string[]]$OnPremDnsServers,
+    [string]$StorageEndpoint,
+    [string]$PrivateDnsIp,
+    [string]$TempUser,
+    [switch]$SkipUserDisable
+)
+
+function Write-OdjBlob {
+    param(
+        [string]$OdjBlob,
+        [string]$Path
+    )
+
+    $byteArray = [System.Byte[]]@()
+    $byteArray += 255
+    $byteArray += 254
+
+    $byteArray += [System.Text.Encoding]::Unicode.GetBytes($OdjBlob)
+
+    $byteArray += 0
+    $byteArray += 0
+
+    $writer = [System.IO.File]::Create($Path)
+    $writer.Write($byteArray, 0, $byteArray.Length)
+
+    $writer.Close()
+    $writer.Dispose()
+}
+
+Install-WindowsFeature `
+        -Name "DNS" `
+        -WarningAction SilentlyContinue | `
+    Out-Null
+
+if ($DomainJoin) {
+    $path = Get-Location | Select-Object -ExpandProperty Path
+    $dnsForwarderOdj = [System.IO.Path]::Combine($path, "dnsforwarder.odj")
+    $djOutput = [System.IO.Path]::Combine($path, "djOutput.txt")
+    
+    Write-OdjBlob -OdjBlob $OdjBlob -Path $dnsForwarderOdj
+    Invoke-Expression `
+            -Command "djoin.exe /requestodj /loadfile `"$dnsForwarderOdj`" /windowspath $($env:windir) /localos" | `
+        Out-File -FilePath $djOutput
+
+    $domainZoneName = Get-DnsServerZone | Where-Object { $_.ZoneName -eq $DomainName }
+    if ($null -eq $domainZoneName) {
+        Add-DnsServerConditionalForwarderZone `
+            -Name $DomainName `
+            -MasterServers $OnpremDnsServers
+    }
+}
+
+$storageZoneName = Get-DnsServerZone | Where-Object { $_.ZoneName -eq $StorageEndpoint }
+if ($null -eq $storageZoneName) {
+    Add-DnsServerConditionalForwarderZone `
+        -Name $StorageEndpoint `
+        -MasterServers $PrivateDnsIp
+}
+
+ipconfig.exe /renew | Out-Null 
+ipconfig.exe /flushdns | Out-Null
+
+if (!$SkipUserDisable) {
+    Disable-LocalUser -Name $TempUser
+}
