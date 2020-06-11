bda9b805cdb56f4cd8e59dfe73dbbf81e15f228e
skille <skille@users.noreply.github.com>
2020-06-11 13:38:13 +0200

    Relative path to module files
    Use $PSScriptRoot as an relative path to the module files if not in current directory.

--- AzFilesHybrid/CopyToPSPath.ps1
+++ AzFilesHybrid/CopyToPSPath.ps1
@@ -3,11 +3,11 @@ if (!(Test-Path -Path $psModPath)) {
     New-Item -Path $psModPath -ItemType Directory | Out-Null
 }
 
-$psdFile = Import-PowerShellDataFile -Path .\AzFilesHybrid.psd1
+$psdFile = Import-PowerShellDataFile -Path $PSScriptRoot\AzFilesHybrid.psd1
 $desiredModulePath = "$psModPath\AzFilesHybrid\$($psdFile.ModuleVersion)\"
 if (!(Test-Path -Path $desiredModulePath)) {
     New-Item -Path $desiredModulePath -ItemType Directory | Out-Null
 }
 
-Copy-Item -Path ".\AzFilesHybrid.psd1" -Destination $desiredModulePath
-Copy-Item -Path ".\AzFilesHybrid.psm1" -Destination $desiredModulePath
+Copy-Item -Path "$PSScriptRoot\AzFilesHybrid.psd1" -Destination $desiredModulePath
+Copy-Item -Path "$PSScriptRoot\AzFilesHybrid.psm1" -Destination $desiredModulePath
