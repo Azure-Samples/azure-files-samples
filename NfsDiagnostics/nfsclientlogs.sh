c0a3f3319a2c6722da6384decbe02bf9386775de
Aman <2020ceb1004@iitrpr.ac.in>
2023-06-22 19:54:38 +0530

    Improved arguments parsing for NFS and CIFS scripts
      * Added meaning variables names to existing variables.
      * Refactored all the executable code inside functions.
      * Reoredered definitions of functions.
      * Added check whether python/python3 is installed.
      * Improved arguments parsing.

--- NfsDiagnostics/nfsclientlogs.sh
+++ NfsDiagnostics/nfsclientlogs.sh
@@ -2,61 +2,116 @@
 
 PIDFILE="/tmp/nfsclientlog.pid"
 DIRNAME="./output"
-# prog file
+NFS_PORT=2049
+TRACE_NFSBPF_ABS_PATH="$(cd "$(dirname "trace-nfsbpf")" && pwd)/$(basename "trace-nfsbpf")"
+PYTHON_PROG='python'
+STDLOG_FILE='/dev/null'
 
-if [ $1 == "v3b" ]
-then
-  nfs_port=111
-else
-  nfs_port=2049
-fi
 
-if [ ! -d "$DIRNAME" ]; then
+main() {
+  if [[ "$*" =~ "v3b" ]]
+  then
+    NFS_PORT=111
+  else
+    NFS_PORT=2049
+  fi
+
+  if [[ "$*" =~ "start" ]]
+  then
+    start "$@" 0<&- > "${STDLOG_FILE}" 2>&1
+  elif [[  "$*" =~ "stop" ]]
+  then
+    stop 0<&- > "${STDLOG_FILE}" 2>&1
+  else
+    echo "Usage: ./nfsclientlogs.sh <v3b | v4> <> <start | stop> <CaptureNetwork> <OnAnomaly>"
+    exit 1
+  fi
+
+  exit $?
+}
+
+start() {
+  init
+  start_trace
+
+  if [[ "$*" =~ "CaptureNetwork" ]]; then
+    capture_network
+  fi
+
+  if [[ "$*" =~ "OnAnomaly" ]]; then
+    trace_nfsbpf
+  fi
+}
+
+init() {
+  check_utils
+  rm -r "$DIRNAME" 
   mkdir -p "$DIRNAME"
-fi
 
-ABSOLUTE_PATH="$(cd "$(dirname "trace-nfsbpf")" && pwd)/$(basename "trace-nfsbpf")"
-progfile2="nohup $ABSOLUTE_PATH"
-
-which trace-cmd > /dev/null
-if [ $? == 1 ]; then
-	echo "trace-cmd is not installed."
-	exit 1
-fi
-
-which zip > /dev/null
-if [ $? == 1 ]; then
-        echo "zip is not installed, please install zip to continue"
-        exit 1
-fi
-
-init() { 
   dmesg -Tc > /dev/null
-  rm -f "${DIRNAME}/nfs_dmesg" 
-  rm -f "${DIRNAME}/nfs_trace"
-  rm -f "${DIRNAME}/nfs_traffic.pcap"
+  # rm -f "${DIRNAME}/nfs_dmesg"
+  # rm -f "${DIRNAME}/nfs_trace"
+  # rm -f "${DIRNAME}/nfs_traffic.pcap"
+}
+
+check_utils() {
+  which trace-cmd > /dev/null
+  if [ $? == 1 ]; then
+    echo "trace-cmd is not installed, please install trace-cmd"
+    exit 1
+  fi
+
+  which zip > /dev/null
+  if [ $? == 1 ]; then
+    echo "zip is not installed, please install zip to continue"
+    exit 1
+  fi
+
+  which python > /dev/null
+  if [ $? == 1 ]; then
+    which python3 > /dev/null
+    if [ $? == 1 ]; then
+      echo "python is not installed, please install python to continue"
+      exit 1
+    else PYTHON_PROG='python3'
+    fi
+  fi
 }
 
 start_trace() {
   rpcdebug -m rpc -s all
   rpcdebug -m nfs -s all
-  trace-cmd start -e nfs 
+
+  trace-cmd start -e nfs
+}
+
+capture_network() {
+  nohup tcpdump -p -s 0 port ${NFS_PORT} -w "${DIRNAME}/nfs_traffic.pcap" &
+  echo $! > "${PIDFILE}"
+}
+
+trace_nfsbpf() {
+  nohup "${PYTHON_PROG}" "${TRACE_NFSBPF_ABS_PATH}" "${DIRNAME}" 0<&- 2>&1 &
+}
+
+stop() {
+  dmesg -T > "${DIRNAME}/nfs_dmesg"
+  stop_trace
+  stop_capture_network
+
+  zip -r "$(basename ${DIRNAME}).zip" "${DIRNAME}"
+  return 0;
 }
 
 stop_trace() {
   trace-cmd extract
   sleep 1
   trace-cmd report > "${DIRNAME}/nfs_trace"
-  trace-cmd stop 
+  trace-cmd stop
   trace-cmd reset
   rpcdebug -m rpc -c all
   rpcdebug -m nfs -c all
-  rm -rf trace.dat* 
-}
-
-capture_network() {
-  nohup tcpdump -p -s 0 port ${nfs_port} -w "${DIRNAME}/nfs_traffic.pcap" &
-  echo $! > "${PIDFILE}"
+  rm -rf trace.dat*
 }
 
 stop_capture_network() {
@@ -70,60 +125,14 @@ stop_capture_network() {
   retry=0
   while [ $? == 0 ] && [ $retry -lt 10 ]
   do
-      retry=$((retry + 1))
-      sleep 1
-      ps -p "${pid}" > /dev/null
-    done
+    retry=$((retry + 1))
+    sleep 1
+    ps -p "${pid}" > /dev/null
+  done
   ps -p "${pid}" > /dev/null
   [ $? == 0 ] && echo "Error closing tcpdump" >&2
   rm -f ${PIDFILE}
 }
 
 
-start() {
-  init
-  start_trace
-
-	if [ "$1" == "CaptureNetwork" ] || [ "$2" == "CaptureNetwork" ]; then
-    capture_network
-		# $progfile1 0<&- > /dev/null 2>&1 &
-	fi
-
-	if [ "$1" == "OnAnomaly" ] || [ "$2" == "OnAnomaly" ]; then
-    echo "OnAnomaly Running"
-		$progfile2 "${DIRNAME}" 0<&-   2>&1 &
-	fi	
-}
-
-stop() {
-  dmesg -T > "${DIRNAME}/nfs_dmesg"
-  stop_trace
-  stop_capture_network
-  zip -r "$(basename ${DIRNAME}).zip" "${DIRNAME}" 
-
-  return 0
-}
-
-case $2 in
-	start)
-		if [ "$3" != "" ]; then
-			if [ "$4" != "" ]; then
-				start $3 $4 0<&- > "${DIRNAME}/stdlog.txt" 2>&1
-			else
-				start $3 0<&- > "${DIRNAME}/stdlog.txt" 2>&1
-
-			fi
-		else
-			start 0<&- > "${DIRNAME}/stdlog.txt" 2>&1
-
-		fi
-		;;
-	stop)
-		stop
-		;;
-	*)
-		echo "Usage: ./nfsclientlogs.sh <v3b | v4> <> <start | stop> <CaptureNetwork> <OnAnomaly>"
-		;;
-esac
-
-exit $?
+main "$@"
--- SMBDiagnostics/smbclientlogs.sh
+++ SMBDiagnostics/smbclientlogs.sh
@@ -1,92 +1,145 @@
 #!/bin/bash
 
-#pid file
-
 PIDFILE="/tmp/smbclientlog.pid"
 DIRNAME="./output"
-# prog file
-progfile="nohup trace-cmd record -e cifs"
+CIFS_PORT=445
+TRACE_CIFSBPF_ABS_PATH="$(cd "$(dirname "trace-cifsbpf")" && pwd)/$(basename "trace-cifsbpf")"
+PYTHON_PROG='python'
+STDLOG_FILE='/dev/null'
 
-if [ ! -d "$DIRNAME" ]; then
+
+main() {
+  if [[ "$*" =~ "start" ]]
+  then
+    start "$@" 0<&- > "${STDLOG_FILE}" 2>&1
+  elif [[  "$*" =~ "stop" ]]
+  then
+    stop 0<&- > "${STDLOG_FILE}" 2>&1
+  else
+    echo "Usage: ./smbclientlogs.sh <start | stop> <CaptureNetwork>"
+    exit 1
+  fi
+
+  exit $?
+}
+
+start() {
+  init
+  start_trace
+  dump_os_information
+  echo "======= Dumping CIFS Debug Stats at start =======" > cifs_diag.txt
+  dump_debug_stats
+  echo "=================================================" >> cifs_diag.txt
+
+
+  if [[ "$*" =~ "CaptureNetwork" ]]; then
+    capture_network
+  fi
+
+  if [[ "$*" =~ "OnAnomaly" ]]; then
+    trace_cifsbpf
+  fi
+}
+
+init() {
+  check_utils
+  rm -r "$DIRNAME" 
   mkdir -p "$DIRNAME"
-fi
 
-# tcpdump
-progfile1="nohup tcpdump -p -s 0 -w cifs_traffic.pcap port 445"
+  dmesg -Tc > /dev/null
+  # rm -f "${DIRNAME}/cifs_dmesg"
+  # rm -f "${DIRNAME}/cifs_trace"
+  # rm -f "${DIRNAME}/cifs_traffic.pcap"
+}
 
-# trace-cifsbpf
-ABSOLUTE_PATH="$(cd "$(dirname "trace-cifsbpf")" && pwd)/$(basename "trace-cifsbpf")"
-progfile2="nohup $ABSOLUTE_PATH"
+check_utils() {
+  which trace-cmd > /dev/null
+  if [ $? == 1 ]; then
+    echo "trace-cmd is not installed, please install trace-cmd"
+    exit 1
+  fi
 
-# Sanity to check whether trace-cmd is installed.
-which trace-cmd > /dev/null
-if [ $? == 1 ]; then
-	echo "trace-cmd is not installed, please install trace-cmd to continue"
-	exit 1
-fi
+  which zip > /dev/null
+  if [ $? == 1 ]; then
+    echo "zip is not installed, please install zip to continue"
+    exit 1
+  fi
 
-# Sanity to check whether zip is installed.
-which zip > /dev/null
-if [ $? == 1 ]; then
-        echo "zip is not installed, please install zip to continue"
-        exit 1
-fi
+  which python > /dev/null
+  if [ $? == 1 ]; then
+    which python3 > /dev/null
+    if [ $? == 1 ]; then
+      echo "python is not installed, please install python to continue"
+      exit 1
+    else PYTHON_PROG='python3'
+    fi
+  fi
+}
 
-dump_debug_stats() {
-	echo -e "\nDate: `date -u`" >> cifs_diag.txt
-	echo -e "\n======= CIFS Stats from /proc/fs/cifs/Stats =======" >> cifs_diag.txt
-	cat /proc/fs/cifs/Stats >> cifs_diag.txt
-	echo -e "\n======= CIFS DebugData from /proc/fs/cifs/DebugData =======" >> cifs_diag.txt
-	cat /proc/fs/cifs/DebugData >> cifs_diag.txt
-	echo -e "\n======= CIFS Open files from /proc/fs/cifs/open_files =======" >> cifs_diag.txt
-	cat /proc/fs/cifs/open_files >> cifs_diag.txt
-	echo -e "\n======= CIFS Mounts =======" >> cifs_diag.txt
-	mount -t cifs >> cifs_diag.txt
-	echo -e "\n======= CIFS TCP Connections =======" >> cifs_diag.txt
-	ss -t | grep microsoft >> cifs_diag.txt
+start_trace() {
+  echo 'module cifs +p' > /sys/kernel/debug/dynamic_debug/control
+  echo 'file fs/cifs/* +p' > /sys/kernel/debug/dynamic_debug/control
+  echo 7 > /proc/fs/cifs/cifsFYI
+  trace-cmd start -e cifs
 }
 
 dump_os_information() {
-	echo "======= Distro details =======" > os_details.txt
-	cat /etc/os-release >> os_details.txt
-	echo -e "\nKernel version: `uname -a`" >> os_details.txt
-	echo -e "\nSMB/CIFS Kernel Module information:" >> os_details.txt
-	modinfo cifs >> os_details.txt
-	echo -e "\nMount.cifs version: `mount.cifs -V`" >> os_details.txt
-	echo -e "\nLast reboot:" >> os_details.txt
-	last reboot -5 >> os_details.txt
-	echo -e "\nSystem Uptime:" >> os_details.txt
-	cat /proc/uptime >> os_details.txt
+  echo "======= Distro details =======" > os_details.txt
+  cat /etc/os-release >> os_details.txt
+  echo -e "\nKernel version: `uname -a`" >> os_details.txt
+  echo -e "\nSMB/CIFS Kernel Module information:" >> os_details.txt
+  modinfo cifs >> os_details.txt
+  echo -e "\nMount.cifs version: `mount.cifs -V`" >> os_details.txt
+  echo -e "\nLast reboot:" >> os_details.txt
+  last reboot -5 >> os_details.txt
+  echo -e "\nSystem Uptime:" >> os_details.txt
+  cat /proc/uptime >> os_details.txt
 }
 
-init() { 
-  dmesg -Tc > /dev/null
-  rm -f "${DIRNAME}/cifs_dmesg" 
-  rm -f "${DIRNAME}/cifs_trace"
-  rm -f "${DIRNAME}/cifs_traffic.pcap"
+dump_debug_stats() {
+  echo -e "\nDate: `date -u`" >> cifs_diag.txt
+  echo -e "\n======= CIFS Stats from /proc/fs/cifs/Stats =======" >> cifs_diag.txt
+  cat /proc/fs/cifs/Stats >> cifs_diag.txt
+  echo -e "\n======= CIFS DebugData from /proc/fs/cifs/DebugData =======" >> cifs_diag.txt
+  cat /proc/fs/cifs/DebugData >> cifs_diag.txt
+  echo -e "\n======= CIFS Open files from /proc/fs/cifs/open_files =======" >> cifs_diag.txt
+  cat /proc/fs/cifs/open_files >> cifs_diag.txt
+  echo -e "\n======= CIFS Mounts =======" >> cifs_diag.txt
+  mount -t cifs >> cifs_diag.txt
+  echo -e "\n======= CIFS TCP Connections =======" >> cifs_diag.txt
+  ss -t | grep microsoft >> cifs_diag.txt
 }
 
 
-start_trace() {
-	echo 'module cifs +p' > /sys/kernel/debug/dynamic_debug/control
-	echo 'file fs/cifs/* +p' > /sys/kernel/debug/dynamic_debug/control
-	echo 7 > /proc/fs/cifs/cifsFYI
-  trace-cmd start -e cifs
+capture_network() {
+  nohup tcpdump -p -s 0 port ${CIFS_PORT} -w "${DIRNAME}/cifs_traffic.pcap" &
+  echo $! > "${PIDFILE}"
+}
+
+trace_cifsbpf() {
+  nohup "${PYTHON_PROG}" "${TRACE_CIFSBPF_ABS_PATH}" "${DIRNAME}" 0<&- 2>&1 &
+}
+
+stop() {
+  dmesg -T > "${DIRNAME}/cifs_dmesg"
+  stop_trace
+  stop_capture_network
+  echo -e "\n\n======= Dumping CIFS Debug Stats at the end =======" >> cifs_diag.txt
+  dump_debug_stats
+  mv cifs_diag.txt "${DIRNAME}"
+  mv os_details.txt "${DIRNAME}"
+  zip -r "$(basename ${DIRNAME}).zip" "${DIRNAME}"
+  return 0;
 }
 
 stop_trace() {
   trace-cmd extract
   sleep 1
   trace-cmd report > "${DIRNAME}/cifs_trace"
-  trace-cmd stop 
+  trace-cmd stop
   trace-cmd reset
-  rm -rf trace.dat* 
-	echo 0 > /proc/fs/cifs/cifsFYI
-}
-
-capture_network() {
-  nohup tcpdump -p -s 0 port 445 -w "${DIRNAME}/cifs_traffic.pcap" &
-  echo $! > "${PIDFILE}"
+  echo 0 > /proc/fs/cifs/cifsFYI
+  rm -rf trace.dat*
 }
 
 stop_capture_network() {
@@ -100,68 +153,14 @@ stop_capture_network() {
   retry=0
   while [ $? == 0 ] && [ $retry -lt 10 ]
   do
-      retry=$((retry + 1))
-      sleep 1
-      ps -p "${pid}" > /dev/null
-    done
+    retry=$((retry + 1))
+    sleep 1
+    ps -p "${pid}" > /dev/null
+  done
   ps -p "${pid}" > /dev/null
   [ $? == 0 ] && echo "Error closing tcpdump" >&2
   rm -f ${PIDFILE}
 }
 
-start() {
-  init
-  start_trace
-	dump_os_information
-	echo "======= Dumping CIFS Debug Stats at start =======" > cifs_diag.txt
-	dump_debug_stats
-	echo "=================================================" >> cifs_diag.txt
 
-
-	if [ "$1" == "CaptureNetwork" ] || [ "$2" == "CaptureNetwork" ]; then
-    capture_network
-		# $progfile1 0<&- > /dev/null 2>&1 &
-	fi
-
-	if [ "$1" == "OnAnomaly" ] || [ "$2" == "OnAnomaly" ]; then
-    echo "OnAnomaly Running"
-		$progfile2 "${DIRNAME}" &
-	fi	
-
-}
-
-stop() {
-  dmesg -T > "${DIRNAME}/cifs_dmesg"
-  stop_trace
-  stop_capture_network
-
-	echo -e "\n\n======= Dumping CIFS Debug Stats at the end =======" >> cifs_diag.txt
-	dump_debug_stats
-  mv cifs_diag.txt "${DIRNAME}"
-  mv os_details.txt "${DIRNAME}"
-  zip -r "$(basename ${DIRNAME}).zip" "${DIRNAME}" 
-	# zip cifs_debug.zip cifs_dmesg cifs_trace cifs_traffic.pcap cifs_diag.txt os_details.txt
-	return 0;
-}
-
-case $1 in
-	start)
-		if [ "$2" != "" ]; then
-			if [ "$3" != "" ]; then
-				start $2 $3 0<&- > "${DIRNAME}/stdlog.txt" 2>&1
-			else
-				start $2 0<&- > "${DIRNAME}/stdlog.txt" 2>&1
-			fi
-		else
-			start 0<&- > "${DIRNAME}/stdlog.txt" 2>&1
-		fi
-		;;
-	stop)
-		stop
-		;;
-	*)
-		echo "Usage: ./smbclientlogs.sh <start | stop> <CaptureNetwork>"
-		;;
-esac
-
-exit $?
+main "$@"
