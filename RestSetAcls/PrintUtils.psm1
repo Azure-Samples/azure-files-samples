bc8a17f09feb6c703efcde56b40b080b7cef46d3
Maxime Kjaer <maximekjaer@microsoft.com>
2024-03-21 12:31:39 -0700

    Move printing code to PrintUtils.psm1

---
+++ RestSetAcls/PrintUtils.psm1
@@ -0,0 +1,69 @@
+function Ask([Parameter(Mandatory=$false)][string] $question)
+{
+    while ($true) {
+        $yn = Read-Host "${question} [Y/n]"
+        $yn = $yn.Trim().ToLower()
+        if ($yn -eq 'n') {
+            return $false
+        } elseif ($yn -eq '' -or $yn -eq 'y') {
+            return $true
+        }
+        Write-Host "Invalid answer '$yn'. Answer with either 'y' or 'n'" -ForegroundColor Red
+    }
+}
+
+function Get-IsPowerShellIse {
+    return $host.Name -eq "Windows PowerShell ISE Host"
+}
+
+function Get-SpecialCharactersPrintable {
+    # Windows Terminal supports it
+    if ($env:WT_SESSION) {
+        return $true
+    }
+
+    # PowerShell 6+ supports it
+    if ($PSVersionTable.PSVersion.Major -ge 6) {
+        return $true
+    }
+
+    # Older versions don't
+    return $false
+}
+
+function Write-DoneHeader {
+    if (Get-SpecialCharactersPrintable) { 
+        $checkmark = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2713", 16))
+        Write-Host "($checkmark) Done: " -ForegroundColor Green -NoNewline
+    } else {
+        Write-Host "Done: " -ForegroundColor Green -NoNewline
+    }
+}
+
+function Write-PartialHeader {
+    if (Get-SpecialCharactersPrintable) { 
+        $cross = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2717", 16))
+        Write-Host "($cross) Partial: " -ForegroundColor Yellow -NoNewline
+    } else {
+        Write-Host "Partial: " -ForegroundColor Yellow -NoNewline
+    }
+}
+
+function Write-FailedHeader {
+    if (Get-SpecialCharactersPrintable) { 
+        $cross = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2717", 16))
+        Write-Host "($cross) Failed: " -ForegroundColor Red -NoNewline
+    } else {
+        Write-Host "Failed: " -ForegroundColor Red -NoNewline
+    }
+}
+
+function Write-WarningHeader {
+    if (Get-SpecialCharactersPrintable) { 
+        $warning = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("26A0", 16))
+        Write-Host "($warning) Warning: " -ForegroundColor Yellow -NoNewline
+    } else {
+        Write-Host "Warning: " -ForegroundColor Yellow -NoNewline
+    }
+
+}
--- RestSetAcls/RestSetAcls.psm1
+++ RestSetAcls/RestSetAcls.psm1
@@ -1,72 +1,4 @@
-function Ask([Parameter(Mandatory=$false)][string] $question)
-{
-    while ($true) {
-        $yn = Read-Host "${question} [Y/n]"
-        $yn = $yn.Trim().ToLower()
-        if ($yn -eq 'n') {
-            return $false
-        } elseif ($yn -eq '' -or $yn -eq 'y') {
-            return $true
-        }
-        Write-Host "Invalid answer '$yn'. Answer with either 'y' or 'n'" -ForegroundColor Red
-    }
-}
-
-function Get-IsPowerShellIse {
-    return $host.Name -eq "Windows PowerShell ISE Host"
-}
-
-function Get-SpecialCharactersPrintable {
-    # Windows Terminal supports it
-    if ($env:WT_SESSION) {
-        return $true
-    }
-
-    # PowerShell 6+ supports it
-    if ($PSVersionTable.PSVersion.Major -ge 6) {
-        return $true
-    }
-
-    # Older versions don't
-    return $false
-}
-
-function Write-DoneHeader {
-    if (Get-SpecialCharactersPrintable) { 
-        $checkmark = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2713", 16))
-        Write-Host "($checkmark) Done: " -ForegroundColor Green -NoNewline
-    } else {
-        Write-Host "Done: " -ForegroundColor Green -NoNewline
-    }
-}
-
-function Write-PartialHeader {
-    if (Get-SpecialCharactersPrintable) { 
-        $cross = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2717", 16))
-        Write-Host "($cross) Partial: " -ForegroundColor Yellow -NoNewline
-    } else {
-        Write-Host "Partial: " -ForegroundColor Yellow -NoNewline
-    }
-}
-
-function Write-FailedHeader {
-    if (Get-SpecialCharactersPrintable) { 
-        $cross = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2717", 16))
-        Write-Host "($cross) Failed: " -ForegroundColor Red -NoNewline
-    } else {
-        Write-Host "Failed: " -ForegroundColor Red -NoNewline
-    }
-}
-
-function Write-WarningHeader {
-    if (Get-SpecialCharactersPrintable) { 
-        $warning = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("26A0", 16))
-        Write-Host "($warning) Warning: " -ForegroundColor Yellow -NoNewline
-    } else {
-        Write-Host "Warning: " -ForegroundColor Yellow -NoNewline
-    }
-
-}
+Import-Module $PSScriptRoot/PrintUtils.psm1
 
 function Get-AreAceFlagsInSddlAllEqualToTarget {
     param (
@@ -463,7 +395,8 @@ function Set-AzureFilesAclRecursive {
             -InheritanceFlags $targetInheritanceFlags `
             -PropagationFlags $targetPropagationFlags
         
-        Write-Host "The SDDL string has non-standard inheritance rules." -ForegroundColor Yellow
+        Write-WarningHeader
+        Write-Host "The SDDL string has non-standard inheritance rules."
         Write-Host "It is recommended to set OI (Object Inherit) and CI (Container Inherit) on every permission. " -ForegroundColor DarkGray
         Write-Host "This ensures that the permissions are inherited by files and folders created in the future." -ForegroundColor DarkGray
         Write-Host
