a8e10d48f6fe5bbc33762c2ee53a90e5cb2129e6
Suruchi Maini <134455898+MainiSuruchi@users.noreply.github.com>
2024-09-20 15:08:40 -0700

    CheckEntraJoinType name updated for checkHAADJ/AADJ

--- AzFilesHybrid/AzFilesHybrid.psm1
+++ AzFilesHybrid/AzFilesHybrid.psm1
@@ -3842,7 +3842,7 @@ function Debug-AzStorageAccountEntraKerbAuth {
             "CheckAdminConsent" = [CheckResult]::new("CheckAdminConsent");
             "CheckWinHttpAutoProxySvc" = [CheckResult]::new("CheckWinHttpAutoProxySvc");
             "CheckIpHlpScv" = [CheckResult]::new("CheckIpHlpScv");
-            "CheckforHAADJ/AADJ" = [CheckResult]::new("CheckforHAADJ/AADJ")
+            "CheckEntraJoinType" = [CheckResult]::new("CheckEntraJoinType")
         }
         #
         # Port 445 check 
@@ -4125,40 +4125,37 @@ function Debug-AzStorageAccountEntraKerbAuth {
         #
         #Check if the machine is HAADJ or AADJ
         #
-        if (!$filterIsPresent -or $Filter -match "CheckforHAADJ/AADJ")
+        if (!$filterIsPresent -or $Filter -match "CheckEntraJoinType")
         {   
-           try 
-           {
-               $checksExecuted += 1; 
-               $dsregcmd = dsregcmd /status
-               $status = New-Object -TypeName PSObject
-               $dsregcmd | Select-String -Pattern " *[A-z]+ : [A-z]+ *" | ForEach-Object 
-               {
-                Add-Member -InputObject $status -MemberType NoteProperty -Name (([String]$_).Trim() -split " : ")[0] -Value (([String]$_).Trim() -split " : ")[1]
-                }
+            try 
+            {
+                $checksExecuted += 1; 
+                $status = Get-DsRegStatus
                 
-               if(($status.AzureAdJoined -eq "YES") -and ($status.DomainJoined -eq "NO"))
-               {
-                Write-Host "It is an AAD Joined machine"
-                $checks["CheckforHAADJ/AADJ"].Result = "Passed"
-               }
+                if ($status.AzureAdJoined -eq "YES")
+                {
+                    if ($status.DomainJoined -eq "NO")
+                    {
+                        Write-Host "It is an Entra Joined machine"
+                    }
+                    elseif ($status.DomainJoined -eq "YES")
+                    {
+                        Write-Host "It is an Hybrid Entra Joined machine"
+                    }
 
-               if(($status.AzureAdJoined -eq "YES") -and ($status.DomainJoined -eq "YES"))
-               {
-                Write-Host "It is an Hybrid AAD Joined machine"
-                $checks["CheckforHAADJ/AADJ"].Result = "Passed"
-               }
-               else 
-               {
-                Write-Error "Entra Kerb requires AADJ or HAADJ machine."
-                $checks["CheckforHAADJ/AADJ"].Result = "Failed"
-               }
+                    $checks["CheckEntraJoinType"].Result = "Passed"
+                }
+                else
+                {
+                    $checks["CheckEntraJoinType"].Result = "Failed"
+                    Write-Error "Entra Kerb requires AADJ or HAADJ machine."
+                }
             }
             catch 
             {
-                $checks["CheckforHAADJ/AADJ"].Result = "Failed"
-                $checks["CheckforHAADJ/AADJ"].Issue = $_
-                Write-Error "CheckforHAADJ/AADJ - FAILED"
+                $checks["CheckEntraJoinType"].Result = "Failed"
+                $checks["CheckEntraJoinType"].Issue = $_
+                Write-Error "CheckEntraJoinType - FAILED"
                 Write-Error $_
             }
 
@@ -4166,6 +4163,28 @@ function Debug-AzStorageAccountEntraKerbAuth {
 
         SummaryOfChecks -filterIsPresent $filterIsPresent -checksExecuted $checksExecuted
     }
+}
+
+function Get-DsRegStatus {
+    $dsregcmd = dsregcmd /status
+    $status = New-Object -TypeName PSObject
+    $dsregcmd `
+        | Select-String -Pattern " *[A-z]+ : [A-z]+ *" `
+        | ForEach-Object {
+            $parts = ([String]$_).Trim() -split " : "
+            $key = $parts[0]
+            $value = $parts[1]
+
+            if (-not (Get-Member -inputobject $status -name $key -Membertype Properties)) {
+                Add-Member `
+                    -InputObject $status `
+                    -MemberType NoteProperty `
+                    -Name $key `
+                    -Value $value
+            }
+        }
+
+    return $status
 }
 
 function Debug-EntraKerbAdminConsent {
