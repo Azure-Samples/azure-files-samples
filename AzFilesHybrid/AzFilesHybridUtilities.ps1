0fe1f6193419f9b4b52c4fe5026904824327bbd7
Valerio Marcelli <v-vmarcelli@microsoft.com>
2024-12-04 16:29:58 -0800

    added utility functions for new output

--- AzFilesHybrid/AzFilesHybridUtilities.ps1
+++ AzFilesHybrid/AzFilesHybridUtilities.ps1
@@ -2,7 +2,7 @@ function Write-FailedPSStyle(
     [Parameter(Mandatory=$true, Position=0)]
     [string]$message
 ) {
-    [string]$redFailed = "`t$($PSStyle.Foreground.BrightRed)FAILED$($PSStyle.Reset)"
+    [string]$redFailed = "`t$($PSStyle.Foreground.BrightRed)ERROR$($PSStyle.Reset)"
     Write-Host "${redFailed}: $message"
 }
 
@@ -12,4 +12,19 @@ function Write-WarningPSStyle(
 ) {
     [string]$warning = "`t$($PSStyle.Foreground.BrightYellow)WARNING$($PSStyle.Reset)"
     Write-Host "${warning}: $message"
+}
+
+function Write-TestingPassedPSStyle() {
+    $checkmark = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2713", 16))
+    Write-Host "`r$($PSStyle.Foreground.BrightGreen)`t`t($checkmark) Passed$($PSStyle.Reset)"
+}
+
+function Write-TestingFailedPSStyle() {
+    $cross = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2715", 16))
+    Write-Host "`r$($PSStyle.Foreground.BrightRed)`t`t($cross) Failed$($PSStyle.Reset)"
+}
+
+function Write-TestingWarningPSStyle() {
+    $warning = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("26A0", 16))
+    Write-Host "`r$($PSStyle.Foreground.BrightYellow)`t`t($warning ) Partial$($PSStyle.Reset)"
 }
