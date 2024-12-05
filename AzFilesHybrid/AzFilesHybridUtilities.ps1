f428e61748fa60909e5b7f20e8b3edf6fc74f0cd
Valerio Marcelli <v-vmarcelli@microsoft.com>
2024-12-05 10:19:09 -0800

    removed unnecessary parenthesis

--- AzFilesHybrid/AzFilesHybridUtilities.ps1
+++ AzFilesHybrid/AzFilesHybridUtilities.ps1
@@ -14,17 +14,17 @@ function Write-WarningPSStyle(
     Write-Host "${warning}: $message"
 }
 
-function Write-TestingPassedPSStyle() {
+function Write-TestingPassedPSStyle {
     $checkmark = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2713", 16))
     Write-Host "`r$($PSStyle.Foreground.BrightGreen)`t`t($checkmark) Passed$($PSStyle.Reset)"
 }
 
-function Write-TestingFailedPSStyle() {
+function Write-TestingFailedPSStyle {
     $cross = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2715", 16))
     Write-Host "`r$($PSStyle.Foreground.BrightRed)`t`t($cross) Failed$($PSStyle.Reset)"
 }
 
-function Write-TestingWarningPSStyle() {
+function Write-TestingWarningPSStyle {
     $warning = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("26A0", 16))
     Write-Host "`r$($PSStyle.Foreground.BrightYellow)`t`t($warning ) Partial$($PSStyle.Reset)"
 }
