19b061d1ded1a13a904efaa465aeb19f8c09bd22
Valerio Marcelli <v-vmarcelli@microsoft.com>
2024-12-06 14:15:25 -0800

    refactored two utility functions into one
    combined Write-TestingFailedPSStyle and Write-FailedPSStyle

--- AzFilesHybrid/AzFilesHybridUtilities.ps1
+++ AzFilesHybrid/AzFilesHybridUtilities.ps1
@@ -29,13 +29,19 @@ function Write-TestingPassedPSStyle(
     Write-Host "`r$($PSStyle.Foreground.BrightGreen)${indentation}($checkmark) Passed$($PSStyle.Reset)"
 }
 
-function Write-TestingFailedPSStyle(
-    [Parameter(Mandatory=$false, Position=0)]
-    [int]$Indents = 2
+function Write-TestingFailed(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$Message,
+    [Parameter(Mandatory=$false, Position=1)]
+    [int]$ResultIndent = 2,
+    [Parameter(Mandatory=$false, Position=2)]
+    [int]$ErrorIndent = 1
 ) {
-    $indentation = "`t" * $Indents
+    $resultIndentation = "`t" * $ResultIndent
+    $errorIndentation = "`t" * $ErrorIndent
     $cross = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2715", 16))
-    Write-Host "`r$($PSStyle.Foreground.BrightRed)${indentation}($cross) Failed$($PSStyle.Reset)"
+    Write-Host "`r$($PSStyle.Foreground.BrightRed)${resultIndentation}($cross) Failed$($PSStyle.Reset)"
+    Write-Host "${errorIndentation}$($PSStyle.Foreground.BrightRed)ERROR$($PSStyle.Reset): $Message"
 }
 
 function Write-TestingWarningPSStyle(
