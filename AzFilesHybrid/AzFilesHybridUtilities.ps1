7da2c2e930218c1f5214051671b9f74b5e7374d8
Valerio Marcelli <v-vmarcelli@microsoft.com>
2024-12-09 12:59:37 -0800

    reworked indents, and output

--- AzFilesHybrid/AzFilesHybridUtilities.ps1
+++ AzFilesHybrid/AzFilesHybridUtilities.ps1
@@ -11,33 +11,37 @@ function Write-WarningPSStyle(
 
 function Write-TestingPassedPSStyle(
     [Parameter(Mandatory=$false, Position=0)]
-    [int]$Indents = 4
+    [int]$Indents = 1
 ) {
     $indentation = "`t" * $Indents
     $checkmark = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2713", 16))
-    Write-Host "`r$($PSStyle.Foreground.BrightGreen)${indentation}($checkmark) Passed$($PSStyle.Reset)"
+    Write-Host "$($PSStyle.Foreground.BrightGreen)${indentation}($checkmark) Passed$($PSStyle.Reset)"
 }
 
 function Write-TestingFailed(
     [Parameter(Mandatory=$true, Position=0)]
     [string]$Message,
     [Parameter(Mandatory=$false, Position=1)]
-    [int]$ResultIndent = 4,
+    [int]$ResultIndent = 1,
     [Parameter(Mandatory=$false, Position=2)]
-    [int]$ErrorIndent = 4
+    [int]$ErrorIndent = 1
 ) {
     $resultIndentation = "`t" * $ResultIndent
     $errorIndentation = "`t" * $ErrorIndent
     $cross = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2715", 16))
-    Write-Host "`r$($PSStyle.Foreground.BrightRed)${resultIndentation}($cross) Failed$($PSStyle.Reset)"
+    Write-Host "$($PSStyle.Foreground.BrightRed)${resultIndentation}($cross) Failed$($PSStyle.Reset)"
     Write-Host "${errorIndentation}$($PSStyle.Foreground.BrightRed)ERROR$($PSStyle.Reset): $Message"
 }
 
-function Write-TestingWarningPSStyle(
-    [Parameter(Mandatory=$false, Position=0)]
-    [int]$Indents = 2
+function Write-TestingWarning(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$Message,
+    [Parameter(Mandatory=$false, Position=1)]
+    [int]$Indents = 1
 ) {
     $indentation = "`t" * $Indents
-    $warning = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("26A0", 16))
-    Write-Host "`r$($PSStyle.Foreground.BrightYellow)${indentation}($warning ) Partial$($PSStyle.Reset)"
+    $warningIcon = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("26A0", 16))
+    [string]$warning = "$($PSStyle.Foreground.BrightYellow)WARNING$($PSStyle.Reset)"
+    Write-Host "$($PSStyle.Foreground.BrightYellow)${indentation}($warningIcon ) Partial$($PSStyle.Reset)"
+    Write-Host "${indentation}${warning}: $Message"
 }
