7ad7865e90d5634cbb9b6afb91838dbd242eddfd
Valerio Marcelli <v-vmarcelli@microsoft.com>
2024-12-10 11:49:34 -0800

    removed unneeded indentation param

--- AzFilesHybrid/AzFilesHybridUtilities.ps1
+++ AzFilesHybrid/AzFilesHybridUtilities.ps1
@@ -9,28 +9,19 @@ function Write-TestingPassed(
 
 function Write-TestingFailed(
     [Parameter(Mandatory=$true, Position=0)]
-    [string]$Message,
-    [Parameter(Mandatory=$false, Position=1)]
-    [int]$ResultIndent = 1,
-    [Parameter(Mandatory=$false, Position=2)]
-    [int]$ErrorIndent = 1
+    [string]$Message
 ) {
-    $resultIndentation = "`t" * $ResultIndent
-    $errorIndentation = "`t" * $ErrorIndent
     $cross = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("2715", 16))
-    Write-Host "$($PSStyle.Foreground.BrightRed)${resultIndentation}($cross) Failed$($PSStyle.Reset)"
-    Write-Host "${errorIndentation}$($PSStyle.Foreground.BrightRed)ERROR$($PSStyle.Reset): $Message"
+    Write-Host "$($PSStyle.Foreground.BrightRed)`t($cross) Failed$($PSStyle.Reset)"
+    Write-Host "`t$($PSStyle.Foreground.BrightRed)ERROR$($PSStyle.Reset): $Message"
 }
 
 function Write-TestingWarning(
     [Parameter(Mandatory=$true, Position=0)]
-    [string]$Message,
-    [Parameter(Mandatory=$false, Position=1)]
-    [int]$Indents = 1
+    [string]$Message
 ) {
-    $indentation = "`t" * $Indents
     $warningIcon = [System.Char]::ConvertFromUtf32([System.Convert]::ToInt32("26A0", 16))
     [string]$warning = "$($PSStyle.Foreground.BrightYellow)WARNING$($PSStyle.Reset)"
-    Write-Host "$($PSStyle.Foreground.BrightYellow)${indentation}($warningIcon ) Partial$($PSStyle.Reset)"
-    Write-Host "${indentation}${warning}: $Message"
+    Write-Host "$($PSStyle.Foreground.BrightYellow)`t($warningIcon ) Partial$($PSStyle.Reset)"
+    Write-Host "`t${warning}: $Message"
 }
