00b4240ae5445be084fb4a4e70450d591d5125af
Maxime Kjaer <maximekjaer@microsoft.com>
2025-01-08 07:33:39 -0800

    Add script to test and publish

---
+++ AzFilesHybrid/init.ps1
@@ -0,0 +1,20 @@
+function Test-Manifest {
+    Test-ModuleManifest -Path $PSScriptRoot\AzFilesHybrid\AzFilesHybrid.psd1
+}
+
+function Publish-PSGallery {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = "PowerShell Gallery API Key")]
+        [string]$apiKey
+    )
+
+    Write-Host "Testing manifest" -ForegroundColor White -NoNewline
+    Test-Manifest
+    Write-Host
+
+    # This will also run Test-ModuleManifest
+    Write-Host "Publishing" -ForegroundColor White
+    Publish-Module -Path $PSScriptRoot\RestSetAcls -NuGetApiKey $apiKey -WhatIf:$WhatIfPreference
+    Write-Host "Done" -ForegroundColor Green
+}
