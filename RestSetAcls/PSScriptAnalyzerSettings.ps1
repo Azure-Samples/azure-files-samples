660b6a55e3253f8d0d863147efa4eebfa3c76ac6
Maxime Kjaer <maximekjaer@microsoft.com>
2025-02-12 12:25:27 -0800

    Add type compatibility linter check

--- RestSetAcls/PSScriptAnalyzerSettings.ps1
+++ RestSetAcls/PSScriptAnalyzerSettings.ps1
@@ -23,8 +23,21 @@
 
             # Lists the PowerShell platforms we want to check compatibility with
             TargetProfiles = @(
-                # PowerShell 7.0 on Windows 10.0.14393 
+                # PowerShell 7.0 on Windows 10.0.14393
                 'win-8_x64_10.0.14393.0_7.0.0_x64_3.1.2_core'
+            )
+        }
+
+        PSUseCompatibleTypes = @{
+            Enable = $true
+             # Lists the PowerShell platforms we want to check compatibility with
+             # See: https://learn.microsoft.com/en-us/powershell/utility-modules/psscriptanalyzer/rules/usecompatibletypes?view=ps-modules
+             TargetProfiles = @(
+                'win-8_x64_10.0.14393.0_5.1.14393.2791_x64_4.0.30319.42000_framework', # PowerShell 5.1 on Windows Server 2016
+                'win-8_x64_10.0.17763.0_5.1.17763.316_x64_4.0.30319.42000_framework', # PowerShell 5.1 on Windows Server 2019
+                'win-48_x64_10.0.17763.0_5.1.17763.316_x64_4.0.30319.42000_framework', # PowerShell 5.1 on Windows 10 Pro
+                'win-8_x64_10.0.14393.0_6.2.4_x64_4.0.30319.42000_core', # PowerShell 6.2 on Windows 10.0.14393
+                'win-8_x64_10.0.14393.0_7.0.0_x64_3.1.2_core' # PowerShell 7.0 on Windows 10.0.14393 
             )
         }
     }
