c53b3efd70f07a0e73f5cc116ee65e732394459a
Maxime Kjaer <maximekjaer@microsoft.com>
2025-02-12 12:15:54 -0800

    Add PS compatibility checks to linter

--- RestSetAcls/PSScriptAnalyzerSettings.ps1
+++ RestSetAcls/PSScriptAnalyzerSettings.ps1
@@ -4,4 +4,28 @@
         'PSAvoidUsingWriteHost',
         'PSAvoidTrailingWhitespace'
     )
+
+    Rules = @{
+        # Ensure we are using PowerShell syntax that is compatible
+        # across multiple versions of PowerShell.
+        PSUseCompatibleSyntax = @{
+            Enable = $true
+            
+            # List the targeted versions of PowerShell here
+            TargetVersions = @(
+                '5.1',
+                '7.0'
+            )
+        }
+
+        PSUseCompatibleCommands = @{
+            Enable = $true
+
+            # Lists the PowerShell platforms we want to check compatibility with
+            TargetProfiles = @(
+                # PowerShell 7.0 on Windows 10.0.14393 
+                'win-8_x64_10.0.14393.0_7.0.0_x64_3.1.2_core'
+            )
+        }
+    }
 }
